243f58f845f4168dcb525308e72ab5eeca770acf
KonstantinAvdeenko <AvdeenkoKonstantin1995@gmail.com>
2016-03-23 14:54:07 +0400

    Add my finding the arithmetic mean of the elements ending in five

---
+++ Module1.vb
@@ -0,0 +1,28 @@
+﻿Private Sub CommandButton1_Click()
+'Заполнение массива'
+For i = 1 To 30
+Cells(1, i) = Int((2000 * Rnd) - 1000)
+Next i
+End Sub
+
+Private Sub CommandButton2_Click()
+'Рассчитывает и выводит среднее арифметическое из элементов оканчивающихся на 5'
+k = 0
+a = 0
+For i = 1 To 30
+If Cells(1, i) Mod 10 = 5 Then
+a = a + Cells(1, i)
+k = k + 1
+ElseIf Cells(1, i) Mod 10 = -5 Then
+a = a + Cells(1, i)
+k = k + 1
+End If
+Next i
+b = a / k
+MsgBox (b)
+End Sub
+
+Private Sub CommandButton3_Click()
+'Закрытие формы'
+UserForm1.Hide
+End Sub
---
+++ READ_ME_ENG.md
@@ -0,0 +1,9 @@
+﻿
+This program is in the VBA programming language,
+calculates and derives the arithmetic average of the elements ending in 5.
+The main module Module1 contains the following main functions corresponding to the main buttons on the form:
+1) button fill array - fills array elements with random ones
+positive and negative values ​​from -1000 to 1000;
+2) the button display the arithmetic average - calculates and displays the arithmetic average
+from elements ending in 5;
+3) complete button - closes the form.
